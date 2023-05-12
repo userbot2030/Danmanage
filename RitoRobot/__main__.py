83760cfed001a659ff9f6332715f24c208665b23
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-05-12 13:34:08 +0700

    Update __main__.py

--- RitoRobot/__main__.py
+++ RitoRobot/__main__.py
@@ -555,8 +555,9 @@ def Rito_about_callback(update, context):
                  ]
                 ]
              ), 
-          ) 
-      elif query.data == "Rito_donasi":
+          )
+        
+     elif query.data == "Rito_donasi":
         query.message.edit_text(
             text="ᴅᴏɴᴀꜱɪ ᴅᴇɴɢᴀɴ ꜱᴄᴀɴ Qʀɪꜱ ᴅɪᴀᴛᴀꜱ [🔥](https://telegra.ph/file/bdf23d4e78c8337249c26.png)"
             "\n\n𝙏𝙚𝙧𝙞𝙢𝙖𝙠𝙖𝙨𝙞𝙝 𝙮𝙖𝙣𝙜 𝙨𝙪𝙙𝙖𝙝 𝙗𝙚𝙧𝙙𝙤𝙣𝙖𝙨𝙞🙏",
