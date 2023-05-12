a347f8460ef6497588de6bb04a6bd537e08e895a
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-05-12 15:33:12 +0700

    Update dzstore.py

--- MsRobot/dzstore.py
+++ MsRobot/dzstore.py
@@ -6,4 +6,6 @@ text_dzstore="""🄳🅉 𝗦†♢RΣ ᴅᴀᴘᴀᴛ ᴍᴇʟᴀʏᴀɴɪ ʙ�
 
 [🛒](https://telegra.ph/file/4fd088f75ffa722dbc0aa.png)"""
 
-text_games="""
+text_games="ꜱɪʟᴀᴋᴀɴ ᴘɪʟɪʜ ᴍᴇɴᴜ ɢᴀᴍᴇꜱ ᴅɪʙᴀᴡᴀʜ"
+
+text_ml="""
