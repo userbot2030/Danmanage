fe725457c8eeca76a62d69448e98e306a6fbc260
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-23 22:22:55 +0700

    Update misc.py

--- RitoRobot/modules/helper_funcs/misc.py
+++ RitoRobot/modules/helper_funcs/misc.py
@@ -88,11 +88,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "⋖", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "«", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="Rito_support"),
                 EqInlineKeyboardButton(
-                    "⋗", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "»", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
