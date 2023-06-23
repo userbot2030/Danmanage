4ca2eaca5bdc2bfc833e978f7213e09436b5890f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-23 22:25:59 +0700

    Add files via upload

---
+++ MsRobot/manage.py
@@ -0,0 +1,78 @@
+
+DASAR = """
+**Perintah Dasar**
+
+👮🏻 Tersedia untuk Admin & Moderator
+🕵🏻 Tersedia untuk Admin
+
+👮🏻 `/reload` memperbaru daftar Admin dan hak istimewanya
+
+🕵🏻 `/settings` anda dapat mengelola semua pengaturan Bot di grup
+
+🕵🏻 `/ban` anda dapat memblokir pengguna dari grup tanpa memberinya kemungkinan untuk bergabung kembali menggunakan tautan grup
+
+👮🏻 `/mute` menempatkan pengguna dalam mode hanya-membaca. Dia bisa membaca tetapi tidak bisa mengirim pesan apapun
+
+👮🏻 `/kick` menendang pengguna dari grup, memberinya kemungkinan untuk bergabung kembali menggunakan tautan grup
+
+👮🏻 `/unban` menghapus blokiran pengguna dari grup dalam daftar blokiran, memberinya kemungkinan untuk bergabung kembali dengan tautan grup
+
+👮🏻 `/info` memberikan informasi tentang pengguna
+
+◽️ `/staff` memberikan Daftar lengkap Staf grup
+"""
+
+LANJUT = """
+**Perintah Lanjutan**
+
+🕵🏻 Tersedia untuk Admin
+👮🏻 Tersedia untuk Admin & Moderator
+🛃 Tersedia untuk Admin & Pembersih Obrolan
+
+**MANAJEMEN PERINGATAN**
+👮🏻 `/warn` memberikan peringatan ke pengguna
+👮🏻 `/resetwarn` menghapus 
+👮🏻 `/warnlist` memungkinkan anda melihat dan mengelola peringatan pengguna
+🕵🏻 `/dwarn` menghapus pesan dan memberikan peringatan ke pengguna
+
+🛃 `/del` menghapus pesan yang dipilih
+🛃 `/purge` sama seperti `/del` lebih efisien
+
+◽️ `/report` atau @admin melaporkan pengguna ke admin
+"""
+
+AHLI = """
+**Perintah Ahli**
+
+👥 Tersedia untuk semua pengguna
+👮🏻 Tersedia untuk Admin & Moderator
+🕵🏻 Tersedia untuk Admin
+
+👥 `/admins` melihat daftar admin
+👮🏻 `/admincache` memulai ulang cache admin
+
+**Pesan Tersemat**
+🕵🏻 `/pin` menyematkan pesan dengan membalas ke pesan.
+🕵🏻 `/unpin` menghapus pesan sematan.
+👥 `/pinned` melihat daftar pin yang ada di group
+"""
+
+PRO = """
+**Perintah pro**
+
+👥 Tersedia untuk semua pengguna
+👮🏻 Tersedia untuk admin & moderator
+🕵🏻 Tersedia untuk admin
+
+**Rules Group**
+👥 `/rules` melihat rules group
+👮🏻 `/setrules` pasang rules digroup
+👮🏻 `/clearrules` menghapus rules
+
+**Note group**
+👮🏻 `/notes` melihat notes digroup
+👮🏻 `/save [note name]` menyimpan note
+👮🏻 `/get [note name]` menampilkan note
+👮🏻 `/clear [note name]` hapus note name
+👮🏻 `/removeallnotes` hapus semua notes
+"""
