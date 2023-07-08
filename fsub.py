e81241d204dd207be31090d2146187c45f73379a
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-08 22:38:22 +0700

    Update fsub.py

--- fsub.py
+++ fsub.py
@@ -5,24 +5,23 @@ from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForb
 
 
 @Client.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
+async def join_channel(bot: Client, msg: Message):
     if not CHANNEL:  
         return
     try:
         try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await bot.get_chat_member(CHANNEL, msg.from_user.id)
         except UserNotParticipant:
             if CHANNEL.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/" + CHANNEL
             else:
-                chat_info = await bot.get_chat(MUST_JOIN)
+                chat_info = await bot.get_chat(CHANNEL)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
                     f"Kamu harus join channel dibawah ini sebelum menggunakan saya. Setelah join coba lagi klik /start",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("JOIN CHANNEL", url="https://t.me/stories_zulll")],
                         [InlineKeyboardButton("JOIN CHANNEL", url=link)]
                     ])
                 )
