f256e2c57b0297ff6ae0078c7e809d75ff30b374
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-20 11:28:45 +0700

    Update config.py

--- RitoRobot/config.py
+++ RitoRobot/config.py
@@ -7,37 +7,37 @@ class Config(object):
 
     CASH_API_KEY = "J1BBEIOV38CZ"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    EVENT_LOGS = (-1001935424604)  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1001795374467)  # Event logs channel to note down important bot level events
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph/file/d3b40cbdcbd84bfca790d.jpg"
+    START_IMG = "https://telegra.ph//file/a679b3ae99ff100437671.jpg"
     
-    DONATE_LINK = "https://t.me/stories_zulll/34"
+    DONATE_LINK = "https://link.dana.id/qr/3akqs26o"
 
-    SUPPORT_CHAT = "envSample"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "SiArab_Support"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = "6049689032:AAH_Nmx-icacIb5FEAolpLdyiUnZjH11SU4"  # Get bot token from @BotFather on Telegram
+    TOKEN = "6515197149:AAEUZdUatsvt_t1ElCqWB2qDl5wEg6Ca9DM"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = "ZMOE8Q6BE25J7BEU"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1337085565  # User id of your telegram account (Must be integer)
+    OWNER_ID = 1948147616  # User id of your telegram account (Must be integer)
 
     #TAMBAHAN
     DATABASE_URL = "postgres://bqlkbhhl:YG-iSQ5u5g-6l2MJ-NRgEi-yPJnq3S-H@rajje.db.elephantsql.com/bqlkbhhl"
-    MONGO_DB_URI = "mongodb+srv://avel:tmp0@aveltmp.nqyqy6h.mongodb.net/aveltmp?retryWrites=true&w=majority"
+    MONGO_DB_URI = "mongodb+srv://doadmin:9r260Iqy437zS1lA@db-mongodb-sgp1-52558-1312a8db.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-52558"
     ARQ_API_KEY = "WMFOGU-ONOVQJ-QOZVEQ-UHVFBD-ARQ"
     ARQ_API_URL = "http://arq.hamker.dev"
     
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = [1814359323]  # User id of sudo users
-    DEV_USERS = [1814359323]  # User id of dev users
-    DEMONS = [1814359323]  # User id of support users
-    TIGERS = [1814359323]  # User id of tiger users
-    WOLVES = [1814359323]  # User id of whitelist users
+    DRAGONS = [1948147616]  # User id of sudo users
+    DEV_USERS = [1948147616]  # User id of dev users
+    DEMONS = [1948147616]  # User id of support users
+    TIGERS = [1948147616]  # User id of tiger users
+    WOLVES = [1948147616]  # User id of whitelist users
 
     ALLOW_CHATS = True
-    OWNER_USERNAME = "MSDZULQRNN"
+    OWNER_USERNAME = "Dhilnihnge"
     ALLOW_EXCL = True
     DEL_CMDS = True
     INFOPIC = True
