2202efcf52df91e8cbcdbfa133e09dcf83b24116
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-20 11:17:11 +0700

    Update tagall.py

--- RitoRobot/modules/tagall.py
+++ RitoRobot/modules/tagall.py
@@ -10,13 +10,15 @@ from RitoRobot import telethn as client
 spam_chats = []
 
 
+emoji_tag = ["😀", "😃",  "😄", "😁", "😆", "😅", "😂", "🤣", "😭", "😗", "😙", "😚", "😘", "🥰", "😍", "🤩", "🥳", "🤗", "🙃", "🙂", "☺️", "😊", "😏", "😌", "😉", "🤭", "😶", "😐", "😑", "😔", "😋", "😛", "😝", "😜", "🤪", "🤔", "🤨", "🧐", "🙄", "😒", "😤", "😠", "🤬", "☹️", "🙁", "😕", "😟", "🥺", "😳", "😬", "🤐", "🤫", "😰", "😨", "😧", "😦", "😮", "😯", "😲", "😱", "🤯", "😢", "😥", "😓", "😞", "😖", "😣", "😩", "😫", "🤤", "🥱", "😴", "😪", "🌛", "🌜", "🌚", "🌝", "🎲", "🧩", "♟", "🎯", "🎳", "🎭", "💕", "💞", "💓", "💗", "💖", "❤️‍🔥", "💔", "🤎", "🤍", "🖤", "❤️", "🧡", "💛", "💚", "💙", "💜", "💘", "💝", "🐵", "🦁", "🐯", "🐱", "🐶", "🐺", "🐻", "🐨", "🐼", "🐹", "🐭", "🐰", "🦊", "🦝", "🐮", "🐷", "🐽", "🐗", "🦓", "🦄", "🐴", "🐸", "🐲", "🦎", "🐉", "🦖", "🦕", "🐢", "🐊", "🐍", "🐁", "🐀", "🐇", "🐈", "🐩", "🐕", "🦮", "🐕‍🦺", "🐅", "🐆", "🐎", "🐖", "🐄", "🐂", "🐃", "🐏", "🐑", "🐐", "🦌", "🦙", "🦥", "🦘", "🐘", "🦏", "🦛", "🦒", "🐒", "🦍", "🦧", "🐪", "🐫", "🐿️", "🦨", "🦡", "🦔", "🦦", "🦇", "🐓", "🐔", "🐣", "🐤", "🐥", "🐦", "🦉", "🦅", "🦜", "🕊️", "🦢", "🦩", "🦚", "🦃", "🦆", "🐧", "🦈", "🐬", "🐋", "🐳", "🐟", "🐠", "🐡", "🦐", "🦞", "🦀", "🦑", "🐙", "🦪", "🦂", "🕷️", "🦋", "🐞", "🐝", "🦟", "🦗", "🐜", "🐌", "🐚", "🕸️", "🐛", "🐾", "🌞", "🤢", "🤮", "🤧", "🤒", "🍓", "🍒", "🍎", "🍉", "🍑", "🍊", "🥭", "🍍", "🍌", "🌶", "🍇", "🥝", "🍐", "🍏", "🍈", "🍋", "🍄", "🥕", "🍠", "🧅", "🌽", "🥦", "🥒", "🥬", "🥑", "🥯", "🥖", "🥐",]
+
 @client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
 @client.on(events.NewMessage(pattern="^@all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
         return await event.respond(
-            "__This command can be use in groups and channels!__"
+            "__ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴅᴀᴘᴀᴛ ᴅɪɢᴜɴᴀᴋᴀɴ ᴅᴀʟᴀᴍ ɢʀᴜᴘ ᴅᴀɴ ᴄʜᴀɴɴᴇʟ!!__"
         )
 
     is_admin = False
@@ -30,10 +32,10 @@ async def mentionall(event):
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can mention all!__")
+        return await event.respond("__ʜᴀɴʏᴀ ᴀᴅᴍɪɴ ʏᴀɴɢ ʙɪꜱᴀ ɴɢᴇᴛᴀɢᴀʟʟ!!__")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__Give me one argument!__")
+        return await event.respond("__ᴋᴀꜱɪʜ ꜱᴀʏᴀ ᴘᴇʀɪɴᴛᴀʜ ʏᴀɴɢ ᴊᴇʟᴀꜱ!__")
     elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
@@ -42,11 +44,11 @@ async def mentionall(event):
         msg = await event.get_reply_message()
         if msg == None:
             return await event.respond(
-                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
+                "__ɢᴜᴀ ɢᴀ ʙɪꜱᴀ ɴɢᴇᴛᴀɢᴀʟʟ ᴘᴇꜱᴀɴ ʏᴀɴɢ ᴜᴅᴀʜ ʟᴀᴍᴀ ʙʟᴏᴋ! (messages which are sent before I'm added to group)__"
             )
     else:
         return await event.respond(
-            "__Reply to a message or give me some text to mention others!__"
+            "__ᴋᴀꜱɪʜ ᴘᴇꜱᴀɴ ᴀᴛᴀᴜ ʀᴇᴘʟʏ ᴋᴇ ᴘᴇꜱᴀɴ ᴋᴀʟᴏ ᴍᴀᴜ ᴛᴀɢᴀʟʟ ʙᴏᴅᴏʜ!!__"
         )
 
     spam_chats.append(chat_id)
@@ -56,10 +58,10 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"[{random.choice(emoji_tag)}](tg://user?id={usr.id})"
         if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
+                txt = f"{usrtxt}\n\n{msg}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
@@ -75,7 +77,7 @@ async def mentionall(event):
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
     if not event.chat_id in spam_chats:
-        return await event.respond("__There is no proccess on going...__")
+        return await event.respond("__ᴜᴅᴀʜ ɢᴀ ᴀᴅᴀ ᴛᴀɢᴀʟʟ ʙᴏᴅᴏʜ...__")
     is_admin = False
     try:
         partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
@@ -87,14 +89,15 @@ async def cancel_spam(event):
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can execute this command!__")
+        return await event.respond("__ᴄᴜᴍᴀɴ ᴀᴅᴍɪɴ ʏᴀɴɢ ʙɪꜱᴀ ɴɢᴀꜱɪʜ ᴘᴇʀɪɴᴛᴀʜ!__")
 
     else:
         try:
             spam_chats.remove(event.chat_id)
         except:
             pass
-        return await event.respond("__Stopped Mention.__")
+        return await event.respond("__ᴛᴀɢᴀʟʟ-ɴʏᴀ ᴜᴅᴀʜ ʙᴇʀᴇɴᴛɪ ᴍᴇᴋ.__")
+
 
 
 __mod_name__ = "Tag-All"
