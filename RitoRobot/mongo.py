e54d3ddef4231eb7921d0c49b89a17b020cf908e
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-20 11:49:24 +0700

    Update mongo.py

--- RitoRobot/mongo.py
+++ RitoRobot/mongo.py
@@ -6,14 +6,14 @@ from pymongo.errors import ServerSelectionTimeoutError
 from RitoRobot import MONGO_DB_URI
 
 
-MONGO_DB = "zul"
+MONGO_DB_URI = "doadmin"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB_URI]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
-db = motor[MONGO_DB]
-db = client["zul"]
+db = motor[MONGO_DB_URI]
+db = client["doadmin"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
