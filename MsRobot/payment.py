f219a589dfc055306895504dfc085c67003dbbfa
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-04-28 00:52:40 +0700

    payment.py

--- MsRobot/payment.py
+++ MsRobot/payment.py
@@ -1,16 +1,15 @@
 #MS DZULQURNAIN
 
-text_payment="""𝙋𝘼𝙔𝙈𝙀𝙉𝙏 𝘿𝙕 𝙎𝙏𝙊𝙍𝙀💸
+text_payment="""𝙋𝘼𝙔𝙈𝙀𝙉𝙏 𝘿𝙄𝙎𝙉𝙀𝙔 𝙎𝙏𝙊𝙍𝙀💸
 
-TESTIMONI : [(KLIK DISINI)](https://t.me/TESTI_DZSTORE)
+TESTIMONI : [(KLIK DISINI)](https://t.me/Disney_storeDan)
 
 Payment💳
-»DANA    : 082137969411
-»OVO       : 082137969411
-»GO-PAY : 082137969411
+»DANA    : 083107520691
 
 ᴄᴀᴛᴀᴛᴀɴ:
 1.ᴘᴍ ᴀᴅᴍɪɴ ꜱᴇʙᴇʟᴜᴍ ᴍᴇɴɢɪʀɪᴍ ᴜᴀɴɢ
 2.ᴛʀᴀɴꜱᴀᴋꜱɪ ʙᴇʀʟᴀᴋᴜ ꜱᴇᴛᴇʟᴀʜ ᴘᴍ ᴀᴅᴍɪɴ
 3.ᴅɪʟᴜᴀʀ ᴘᴍ ᴀᴅᴍɪɴ ᴅɪᴀɴɢɢᴀᴘ ᴅᴏɴᴀꜱɪ
-4.ᴊɪᴋᴀ ᴍɪɴᴀᴛ ʜᴜʙ ᴀᴅᴍɪɴ ᴅɪʙᴀᴡᴀʜ ɪɴɪ"""
+4.ᴊɪᴋᴀ ᴍɪɴᴀᴛ ʜᴜʙ ᴀᴅᴍɪɴ ᴅɪʙᴀᴡᴀʜ ɪɴɪ.
+"""
